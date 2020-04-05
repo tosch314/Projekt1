01e352257f08c071a0ae290e370e3d4432b75831
KingKongCondic <DCondic87@gmail.com>
2020-04-05 14:58:50 +0200

    hab den shit mal gemacht, aber 100% gecheckt hab ich's nicht

--- functions.py
+++ functions.py
@@ -1,4 +1,52 @@
-print ("hello")
-print ("shit")
-print ("jo")
-print ("ochse")
+def find_empty(bo):
+    for i in range(len(bo)):
+        for j in range(len(bo)):
+            if bo[i][j] == 0:
+                #bo[i][j] ="y"
+                return i, j #gibt Zeile/Spalte zurück
+
+def validate(bo, num, pos):
+    #Untersucht die Zeile 
+    for i in range(len(bo)):
+        #bo[pos[0][i]] ist die Zeile und Spalte - sprich startet an Position 0 der ersten Zeile
+        #pos[1]  != i soll er nicht untersuchen, da der Wert in diese Zeile eingefügt wurde, soll sich nur alle anderen "Werte" anschauen
+        if bo[pos[0]][i] == num and pos[1] !=i:
+            return False
+    
+    #Untersucht die Spalte    
+    for i in range(len(bo)):
+        if bo[i][pos[1]] == num and pos[0] !=i:
+            return False
+
+    #Untersucht die Box
+    #--> War mir zu hoch.. pos[0], pos[1] hab ich oben schon nicht 100% gerallt. :/
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x*3, box_x*3 + 3):
+            if bo[i][j] == num and (i,j) != pos:
+                False
+    return True
+
+def solve(bo):
+    find = find_empty(bo)
+    if not find:
+        #wird nichts gefunden, ist das Sudoku gelöst und es wird "True wiedergegeben - nix zu solven?!
+        return True
+    else:
+        #in der Funktion find_empty werden die leeren "Stellen" (Zeile/Spalte) zurückgegeben
+        row, col = find
+
+    for i in range(1, 10):
+        #Hier wird der Wert in die gefundene leeze Zeile eingesetzt
+        if validate(bo, i, (row, col)):
+            bo[row][col] = i
+            #
+            if solve(bo):
+                return True
+
+            bo[row][col] = 0
+
+    return False
+
--- main_script.py
+++ main_script.py
@@ -1,1 +1,40 @@
-abcdefc
+from functions import solve
+import functions
+
+
+sudoku = [
+    [0, 9, 0, 3, 6, 0, 1, 0, 4],
+    [0, 7, 5, 0, 0, 2, 3, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 7],
+    [0, 0, 3, 4, 8, 0, 0, 0, 0],
+    [0, 4, 0, 0, 0, 0, 0, 7, 0],
+    [0, 0, 0, 0, 2, 5, 4, 0, 0],
+    [8, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 7, 8, 0, 0, 6, 3, 0],
+    [6, 0, 4, 0, 3, 1, 0, 2, 0],
+]
+
+#Für das setzen der "-", um das Sudoku besser lesen zu können
+# i !=0, da Schleife mit 0 startet und nicht direkt mit - gestartet werden soll
+# i % 3, da nach jeder dritten "Spalte" die "-" gesetzt werden sollen, für die Lesbarkeit, wo kein Rest ist, wird gesetzt
+def print_sudoku(bo):
+    for i in range(len(bo)): 
+        if i % 3==0 and i != 0:
+            print("- - - - - - - - - - - -")
+    
+        for j in range(len(bo)):
+            if j % 3==0 and j != 0:
+                #ohne "end" wird nach dem | in die nächste Zeile gesprungen. end="" = "stay on the same line"
+                print(" | ", end="")
+        
+            if j == 8:
+                print (bo[i][j])
+            else:
+                print (str(bo[i][j]) + " ", end="")
+
+#find_empty(sudoku)
+print_sudoku(sudoku)
+solve(sudoku)
+print("-------------------------")
+
+print_sudoku(sudoku)
